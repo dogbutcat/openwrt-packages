0e1465db1641b412408aa7c6241078fbff8534f2
dogbutcat <dogbutcat@hotmail.com>
2020-06-03 08:26:07 +0800

    luci-app-moschinadns: update list script fix

--- luci-app-moschinadns/root/usr/share/MosChinaDNS/update_list.sh
+++ luci-app-moschinadns/root/usr/share/MosChinaDNS/update_list.sh
@@ -2,53 +2,55 @@
 PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 IP_URL="https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
 DOMAIN_URL="https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf"
-workdir=$(uci get MosChinaDNS.MosChinaDNS.workdir 2>/dev/null)
+WORKDIR=$(uci get MosChinaDNS.MosChinaDNS.WORKDIR 2>/dev/null)
+TEMPDIR="/tmp/MosChinaDNSupdatelist"
 
 # IP/MASK
 update_ip_list(){
     echo "Updating ip list"
-    curl $IP_URL -o /tmp/MosChinaDNSupdatelist/ip_data 2>/dev/null
-    ipv4=$(cat /tmp/MosChinaDNSupdatelist/ip_data | awk -F '|' '$2 ~ /CN/ && $3 ~ /ipv4/ {print $4"/"32-log($5)/log(2)}' 2>/dev/null)
-    ipv6=$(cat /tmp/MosChinaDNSupdatelist/ip_data | awk -F '|' '$2 ~ /CN/ && $3 ~ /ipv6/ {print $4"/"$5}' 2>/dev/null)
-    if [ "$ipv4" = "" ]; then
+    local tmpipfile="$TEMPDIR/ip_data"
+    local tmpfile="$TEMPDIR/temp_chn.list"
+    curl $IP_URL -o $tmpipfile 2>/dev/null
+    if [ "$(awk 'NR==1 {print}' $tmpipfile 2>/dev/null)" = "" ]; then
         echo "received ipv4 empty body"
         EXIT 2
     fi
-    echo $ipv4 > /tmp/MosChinaDNSupdatelist/temp_chn.list
-    echo $ipv6 >> /tmp/MosChinaDNSupdatelist/temp_chn.list
-    if [ ! -d "$workdir" ]; then
-        echo $workdir" is missing"
+    cat $tmpipfile | awk -F '|' '$2 ~ /CN/ && $3 ~ /ipv6/ {print $4"/"$5}' >> $tmpfile 2>/dev/null
+    cat $tmpipfile | awk -F '|' '$2 ~ /CN/ && $3 ~ /ipv4/ {print $4"/"32-log($5)/log(2)}' > $tmpfile 2>/dev/null
+    if [ ! -d "$WORKDIR" ]; then
+        echo $WORKDIR" is missing"
         EXIT 1
     fi
-    mv -f /tmp/MosChinaDNSupdatelist/temp_chn.list "$workdir/chn.list"
+    mv -f $tmpfile "$WORKDIR/chn.list"
     echo "Updating ip finished"
 }
 
 # DOMAIN
 update_domain_list(){
     echo "Updating domain list"
-    curl -L -k $DOMAIN_URL -o /tmp/MosChinaDNSupdatelist/domain_data 2>/dev/null
-    domain=$(cat /tmp/MosChinaDNSupdatelist/domain_data | awk -F '/' '{print $2}')
-    if [ "$domain" = "" ]; then
+    local tmpdomainfile="$TEMPDIR/domain_data"
+    local tmpfile="$TEMPDIR/temp_chn_domain.list"
+    curl -L -k $DOMAIN_URL -o $tmpdomainfile 2>/dev/null
+    if [ "$(awk 'NR==1 {print}' $tmpdomainfile 2>/dev/null)" = "" ]; then
         echo "received domain empty body"
         EXIT 3
     fi
-    echo $domain > /tmp/MosChinaDNSupdatelist/temp_chn_domain.list
-    mv -f /tmp/MosChinaDNSupdatelist/temp_chn_domain.list "$workdir/chn_domain.list"
+    cat $tmpdomainfile | awk -F '/' '{print $2}' > $tmpfile 2>/dev/null
+    mv -f $tmpfile "$WORKDIR/chn_domain.list"
     echo "Updating domain finished"
 }
 EXIT(){
     rm /var/run/update_list 2>/dev/null
-    rm -rf /tmp/MosChinaDNSupdatelist 2>/dev/null
+    rm -rf $TEMPDIR 2>/dev/null
     [ "$1" != "0" ] && touch /var/run/update_list_error && echo $1 > /var/run/update_list_error
     exit $1
 }
 
 main(){
     touch /var/run/update_list
-    rm -rf /tmp/MosChinaDNSupdatelist 2>/dev/null
+    rm -rf $TEMPDIR 2>/dev/null
     rm /var/run/update_list_error 2>/dev/null
-    mkdir /tmp/MosChinaDNSupdatelist
+    mkdir $TEMPDIR
     update_ip_list
     update_domain_list
     EXIT 0
